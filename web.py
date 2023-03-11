6ed0513c16c91b530f17b58e07995d979719050a
Malebu <busch.marius@web.de>
2023-03-11 10:05:52 +0100

    create a web interface v1

---
+++ web.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+st.title("My Todo App")
+st.subheader("This is my todo App.")
+st.write("This app tracks ur todos")
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="", placeholder ="Add new todo")
